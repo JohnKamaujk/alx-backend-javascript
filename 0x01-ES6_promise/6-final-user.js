9e8570e44295f3cd4016d362ce7cc4a6cad86a2e
JohnKamaujk <johnjkkamau@gmail.com>
2024-01-03 14:20:37 +0300

    :sparkles:feat:Promise allSettled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,20 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+async function handleProfileSignup(firstName, lastName, fileName) {
   const userPromise = signUpUser(firstName, lastName);
   const photoPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([userPromise, photoPromise])
-    .then((results) => results.map((result) => ({
+  try {
+    const results = await Promise.allSettled([userPromise, photoPromise]);
+
+    return results.map((result) => ({
       status: result.status,
-      value:
-        result.status === 'fulfilled' ? result.value : result.reason.message,
-    })));
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }));
+  } catch (error) {
+    return [{ status: 'rejected', value: error.message }];
+  }
 }
+
+export default handleProfileSignup;
