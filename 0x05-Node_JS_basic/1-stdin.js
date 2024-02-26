190d9e7ad202e5cfa2fc9bbaa535b196f0d8fa4c
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-26 21:34:42 +0300

    :white_check_mark:print to stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,8 +6,8 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  let chunk;
-  if ((chunk = process.stdin.read())) {
+  const chunk = process.stdin.read();
+  if ((chunk)) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
 });
