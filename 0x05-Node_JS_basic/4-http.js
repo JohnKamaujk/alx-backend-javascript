afc3bf3028e8b6bd9fb3ddcadde26494b3ba1f39
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-26 22:01:13 +0300

    :white_check_mark:HTTP server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,6 @@
 const http = require('http');
 
 const PORT = 1245;
-const HOST = 'localhost';
 const app = http.createServer();
 
 app.on('request', (_, res) => {
@@ -13,8 +12,8 @@ app.on('request', (_, res) => {
   res.write(Buffer.from(responseText));
 });
 
-app.listen(PORT, HOST, () => {
-  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+app.listen(PORT, () => {
+  process.stdout.write(`Server listening at port ${PORT}\n`);
 });
 
 module.exports = app;
