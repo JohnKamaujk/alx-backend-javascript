30357d8f591f682d9cb1c83addd168a7a215d2bb
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-26 21:59:21 +0300

    :white_check_mark:HTTP server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,15 +1,15 @@
 const http = require('http');
+
 const PORT = 1245;
 const HOST = 'localhost';
-
 const app = http.createServer();
 
 app.on('request', (_, res) => {
   const responseText = 'Hello Holberton School!';
-  res.writeHead(200, {
-    'Content-Type': 'text/plain',
-    'Content-Length': Buffer.byteLength(responseText)
-  });
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
   res.write(Buffer.from(responseText));
 });
 
