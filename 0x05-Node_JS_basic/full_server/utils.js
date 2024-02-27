c7554ea24388b17f02896118c80fc2f5971e5674
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-27 21:34:12 +0300

    :white_check_mark:Full server

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,9 +1,9 @@
-import fs from 'fs';
+import { promises as fs } from 'fs';
 
 /**
  * Reads the data of students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Johnny K <https://github.com/JohnKamaujk>
  * @returns {Promise<{
  *   String: {firstname: String, lastname: String, age: number}[]
  * }>}
@@ -11,41 +11,37 @@ import fs from 'fs';
 const readDatabase = (dataPath) => new Promise((resolve, reject) => {
   if (!dataPath) {
     reject(new Error('Cannot load the database'));
+    return;
   }
-  if (dataPath) {
-    fs.readFile(dataPath, (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-      }
-      if (data) {
-        const fileLines = data.toString('utf-8').trim().split('\n');
-        const studentGroups = {};
-        const dbFieldNames = fileLines[0].split(',');
-        const studentPropNames = dbFieldNames.slice(
-          0,
-          dbFieldNames.length - 1,
-        );
 
-        for (const line of fileLines.slice(1)) {
-          const studentRecord = line.split(',');
-          const studentPropValues = studentRecord.slice(
-            0,
-            studentRecord.length - 1,
-          );
-          const field = studentRecord[studentRecord.length - 1];
-          if (!Object.keys(studentGroups).includes(field)) {
-            studentGroups[field] = [];
-          }
-          const studentEntries = studentPropNames.map((propName, idx) => [
-            propName,
-            studentPropValues[idx],
-          ]);
-          studentGroups[field].push(Object.fromEntries(studentEntries));
+  fs.readFile(dataPath, 'utf-8')
+    .then((data) => {
+      const fileLines = data.trim().split('\n');
+      const studentGroups = {};
+      const dbFieldNames = fileLines[0].split(',');
+      const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+
+      for (const line of fileLines.slice(1)) {
+        const studentRecord = line.split(',');
+        const studentPropValues = studentRecord.slice(
+          0,
+          studentRecord.length - 1,
+        );
+        const field = studentRecord[studentRecord.length - 1];
+        if (!Object.keys(studentGroups).includes(field)) {
+          studentGroups[field] = [];
         }
-        resolve(studentGroups);
+        const studentEntries = studentPropNames.map((propName, idx) => [
+          propName,
+          studentPropValues[idx],
+        ]);
+        studentGroups[field].push(Object.fromEntries(studentEntries));
       }
+      resolve(studentGroups);
+    })
+    .catch(() => {
+      reject(new Error('Cannot load the database'));
     });
-  }
 });
 
 export default readDatabase;
