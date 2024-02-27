d6a2f899ea78052a4374556ac1612a5dd80592af
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-27 18:20:07 +0300

    :white_check_mark:Full server

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const mapRoutes = require("./routes").default;
+const express = require('express');
+const mapRoutes = require('./routes').default;
 
 const app = express();
 const PORT = 1245;
@@ -9,4 +9,4 @@ app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
 });
 
-module.exports = app;
+export default app;
