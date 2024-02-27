5189a8584aa5403d1c11b59a2d66df6720225264
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-27 17:28:50 +0300

    :white_check_mark:Full server

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -4,8 +4,8 @@
  * @param {http.ServerResponse} res - The response object
  */
 class AppController {
-  static getHomepage(req, res) {
-    res.status(200).send('Hello Holberton School!');
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
   }
 }
 
