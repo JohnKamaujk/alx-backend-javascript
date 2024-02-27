a377dee5e45631d23f5d18525130f8bd9c249613
JohnKamaujk <johnjkkamau@gmail.com>
2024-02-27 23:20:57 +0300

    :memo:Docs:Readme update

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -7,32 +7,31 @@ import { promises as fs } from 'fs';
  * @returns {Promise<{ [fieldName: string]: string[] }>} Object containing arrays
  * of first names per field.
  */
-const readDatabase = (dataPath) => new Promise((resolve, reject) => {
-  if (!dataPath) {
-    reject(new Error('Cannot load the database'));
-    return;
-  }
+const readDatabase = async (dataPath) => {
+  try {
+    if (!dataPath) {
+      throw new Error('Cannot load the database');
+    }
 
-  fs.readFile(dataPath, 'utf-8')
-    .then((data) => {
-      const fileLines = data.trim().split('\n');
-      const studentGroups = {};
+    const data = await fs.readFile(dataPath, 'utf-8');
+    const fileLines = data.trim().split('\n');
+    const studentGroups = {};
 
-      for (const line of fileLines.slice(1)) {
-        const studentRecord = line.split(',');
-        const firstName = studentRecord[0]; // First column contains first names
-        const field = studentRecord[studentRecord.length - 1];
+    for (const line of fileLines.slice(1)) {
+      const studentRecord = line.split(',');
+      const firstName = studentRecord[0]; // First column contains first names
+      const field = studentRecord[studentRecord.length - 1];
 
-        if (!studentGroups[field]) {
-          studentGroups[field] = [];
-        }
-        studentGroups[field].push(firstName);
+      if (!studentGroups[field]) {
+        studentGroups[field] = [];
       }
-      resolve(studentGroups);
-    })
-    .catch(() => {
-      reject(new Error('Cannot load the database'));
-    });
-});
+      studentGroups[field].push(firstName);
+    }
+
+    return studentGroups;
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
 
 export default readDatabase;
